ad84b66fcc428d7604d31d53d3cdd0c0d4ac499c
unknown <palmaandradeluis@gmail.com>
2022-08-16 12:25:14 -0500

    first commit

---
+++ useTodo.js
@@ -0,0 +1,56 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer";
+
+const init = () =>{
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+    
+    const initialState = [];
+
+    const [todos, dispatchTodos] = useReducer(todoReducer, initialState, init)
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos) || [])
+    }, [todos])
+    
+
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+
+        dispatchTodos( action );
+    }
+
+    const handleDeleteTodo = (id) => {
+        const action = {
+            type: '[TODO] Remove Todo',
+            payload: id
+        }
+
+        dispatchTodos( action );
+    }
+
+    const handleToggleTodo = (id) => {
+        const action = {
+            type: '[TODO] Toggle Todo',
+            payload: id
+        }
+
+        dispatchTodos( action );
+    }
+
+
+    return ({
+        todos,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+        todosCount: todos.length, 
+        todosPendingCount: todos.filter(todo => !todo.done).length
+  })
+}
+
